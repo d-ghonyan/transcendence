8acf738dbfd1c6502019bf42b2cfc31690f55382
Your Name <you@example.com>
2023-10-06 15:04:56 +0400

    add .vscode

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller(["/barev", "/hello"])
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get("/hello")
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/barev")
+  getKov(): string {
+	  return this.appService.getBarev();
   }
 }
